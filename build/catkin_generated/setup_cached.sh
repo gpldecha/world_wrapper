004f67a0e93939d3455970820d11cf5c6646936f
guillaume <guillaume.dechambrier@epfl.ch>
2015-10-17 15:47:47 +0200

    all

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/guillaume/roscode/catkin_ws/src/world_wrapper/build/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/guillaume/roscode/catkin_ws/src/world_wrapper/build/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/guillaume/roscode/catkin_ws/src/world_wrapper/build/devel/lib:/home/guillaume/roscode/catkin_ws/src/world_wrapper/build/devel/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
+export PATH="/home/guillaume/roscode/catkin_ws/src/world_wrapper/build/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/guillaume/roscode/catkin_ws/src/world_wrapper/build/devel/lib/pkgconfig:/home/guillaume/roscode/catkin_ws/src/world_wrapper/build/devel/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH"
+export PYTHONPATH="/home/guillaume/roscode/catkin_ws/src/world_wrapper/build/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/guillaume/roscode/catkin_ws/src/world_wrapper/build/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/guillaume/roscode/catkin_ws/src/world_wrapper:$ROS_PACKAGE_PATH"
